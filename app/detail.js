e23bbbfeb6fc57ed87d2c851a1b766215290fd80
SofDob99 <153644690+SofDob99@users.noreply.github.com>
2024-01-12 22:11:28 +0100

    Version 1.0

--- app/detail.js
+++ app/detail.js
@@ -9,22 +9,57 @@ window.onload = () => {
     let param = obtenerParam(document.URL);
 
     fetch('assets/data/anuario.json')
-        .then(res => res.json())
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`Error en la solicitud: ${res.status}`);
+            }
+            return res.json();
+        })
         .then(data => {
-
-            let project = data.filter((proj) => {
+            let filter = data.filter((proj) => {
                 return proj.Id == param;
             });
-            let images =  [ project[0].imagenes.slice(0,76), project[0].imagenes.slice(78,154)]
 
-            let estructura=`<h1>${project[0].titulo}</h1>
-                <h2>${project[0].subtitulo}</h2>
-                <section class="detail">
-                    <img src='${images[0]}'>
-                    <img src='${images[1]}'>
+            let project = filter[0];
 
-                </section>
-                `;
+            let images = project.imagenes.split(',');
+
+            let estructura = `
+            <nav>
+            <img class="logo" src="assets/imagenes/esd logo.png" alt="">
+            <a href="index.html"><h1>#Poster 03</h1></a>
+        </nav>
+        <main class="space">
+            <div class="work">
+            <h1>${project.titulo}</h1>
+            <h2>${project.subtitulo}</h2>
+            <section class="info">
+                <h4>${project.linea_investigacion}</h4>
+                <p>${project.palabras_clave}</p>
+                <h4>${project.curso} ${project.especialidad}</h4>
+                <h4>${project.asignatura}</h4>
+            </section>
+            <section class="detail">
+                <p class="descripcion">${project.descripcion}</p>
+                <div class="autor">
+                <h4>${project.nombre_estudiante}</h4>
+                <p>${project.redes_estudiante}</p>
+                </div>
+            </section>
+            </div>
+            <div class="contenedor">
+            <img src='${images[0]}'>
+            <img src='${images[2]}'>
+            <img src='${images[3]}'>
+            </div>
+            </main>
+            `;
             document.body.innerHTML = estructura;
-    })
+        })
+        .catch(error => {
+            console.error('Error de fetch:', error);
+        });
+
+
+
 }
--- app/main.js
+++ app/main.js
@@ -1,39 +1,65 @@
-/** Cargar un fichero JSON */
-
 window.onload = () => {
 
-    let lista = document.querySelector("#usuarios");
+    let gallery = document.querySelector("#gallery");
 
-
-    let mostrarDetalle = (e) =>{
-        window.open(`../detalle.html?id=${e.currentTarget.id}`,'_blank');
+    let mostrarDetalle = (e) => {
+        window.open(`../detalle.html?id=${e.currentTarget.id}`, '_blank');
     }
 
+    var anuario = [];
 
+    //Leer anuario
     fetch('assets/data/anuario.json')
-    .then(res => res.json())
-    .then(data => {
-        let projectNum = 0;
-        data.forEach((project, index) => {
-            if(project.titulo != undefined && project.imagenes != "" && project.imagenes != undefined ){
-                let portada = project.imagenes.slice(0, 76);
-                projectNum++;
+        .then(res => res.json())
+        .then(data => {
+            anuario = data;
+        });
 
-                let item=`<li class="project" id="${project.Id}">
-                        <span>${projectNum}</span>
-                        <img src="${portada}" alt="${project.desc_img1} "/>
-                        <span><strong>${project.titulo}</strong> // ${project.nombre_estudiante}</span>
-                </li>`;
-                lista.innerHTML += item;
-            }
-    });
-})
-    .then(()=>{
-        let users = document.querySelectorAll(".project");
-        users.forEach((user) => {
-            user.addEventListener("click", mostrarDetalle, true)
+    fetch('assets/data/anuario.lineas.json')
+        .then(res => res.json())
+        .then(data => {
+
+            // Bucle
+            data.forEach((linea, index) => {
+                getItems(linea.id, linea.nombre);
+            });
+
         })
-});
+        .then(() => {
+            let users = document.querySelectorAll(".project");
+            users.forEach((user) => {
+                user.addEventListener("click", mostrarDetalle, true)
+            });
+
+            // Configuramos el scroll horizontal para cada sección después de cargar los datos
+            initializeCarousels();
+        });
+
+    function getItems(id, nombre) {
+        const filteredData = anuario.filter(f => f.linea_investigacion == nombre);
+
+        let lista = '';
+
+        filteredData.forEach((project, index) => {
+            if (project.titulo != undefined && project.imagenes != "" && project.imagenes != undefined) {
+
+                let portada = project.imagenes.split(',')[0]; //primera imagen
+
+                let item = `<li class="project" id="${project.Id}" data-mouse-down-at="0">
+                    <img src="${portada}" alt="${project.desc_img1}"/>
+                    <span><strong>${project.titulo}</strong></span>
+                </li>`;
+                lista += item;
+            }
+        });
+
+        gallery.innerHTML += `
+            <section id="investigacion${id}">
+                <h2>${nombre}</h2>
+                <ul id="usuarios${id}" class="usuarios">
+                    ${lista}
+                </ul>
+            </section>`;
+    };
 
 }
-
